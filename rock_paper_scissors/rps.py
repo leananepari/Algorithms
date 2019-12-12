2598c082fb259139d6f33a867bdab854753b6f74
Leana <leananeparidze.com>
2019-12-12 14:19:59 -0800

    implements rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,19 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  output = []
+  choices = ['rock', 'paper', 'scissors']
+
+  def helper(played, rounds):
+    if rounds == 0:
+      output.append(played)
+      return
+    
+    for i in range(3):
+      helper(played + [choices[i]], rounds -1)
+  
+  helper([], n)
+  return output
 
 
 if __name__ == "__main__":
